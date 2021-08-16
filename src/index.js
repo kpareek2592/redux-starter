74862f873ecbe3375307352f05631a5ca03b5a2f
Kaushal Pareek <kaushal.pareek@navexglobal.com>
2021-08-16 09:14:44 +0530

    Function composition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import { compse, pipe } from "lodash/fp";
+
+const str = "This is a string";
+
+const trim = (str) => str.trim();
+const toLowerCase = (str) => str.toLowerCase();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+
+const result = wrapInDiv(toLowerCase(trim(str)));
+console.log(result);
